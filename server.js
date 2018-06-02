6d6c73447bbea827920654ac682a5b8066e4edff
nate-river <l.luffy.river@gmail.com>
2018-06-02 23:18:12 +0800

    new match

--- server.js
+++ server.js
@@ -175,6 +175,100 @@ function get_schedule(callback) {
     });
 }
 
+function get_jishousai(cb) {
+    let urls = [
+        {
+            _gtk: 1334249061,
+            eId: 592,
+            stageId: 1946,
+            gameType: 6
+        },
+        {
+            _gtk: 1334249061,
+            eId: 592,
+            stageId: 1944,
+            gameType: 6,
+        },
+        {
+            _gtk: 1334249061,
+            eId: 592,
+            stageId: 1943,
+            gameType: 6
+        },
+    ];
+    var result = [];
+    async.eachLimit(urls, 1, function (v, callback) {
+        request.post({
+            url: 'https://www.wanplus.com/ajax/event/shedule/detail',
+            formData: v,
+            headers: {
+                origin: 'https://www.wanplus.com',
+                cookie: 'wanplus_token=be4542ed8f9d17fae27e44ffb26fa9fd; wanplus_storage=lf4m67eka3o; wanplus_sid=197a65c2e86aa18ab8c7e99f7cef9d90; isShown=1; wp_pvid=3550652531; wanplus_csrf=_csrf_tk_1794037024; wp_info=ssid=s1561235728; Hm_lvt_f69cb5ec253c6012b2aa449fb925c1c2=1521854709,1521860898,1521876181,1521887402; gameType=6; Hm_lpvt_f69cb5ec253c6012b2aa449fb925c1c2=1521897556',
+                referer: 'https://www.wanplus.com/event/592.html',
+                'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Safari/537.36'
+            }
+        }, (err, header, body) => {
+            if (err) {
+                callback(null)
+            } else {
+                let $ = cheerio.load(body);
+                $('.fix_list .war').each((index, el) => {
+                    let id = Number(path.basename($(el).find('.teams a').attr('href'), '.html'));
+                    let date = (() => {
+                        let t = $(el).find('.NO .time').map((index, v) => $(v).text()).get();
+                        if (t.length) {
+                            t.unshift('2018');
+                            return moment(t.join('-'), 'YYYY-MM-DD-HH:mm')
+                        } else {
+                            return moment();
+                        }
+                    })();
+                    let logos = $(el).find('.teamlogo img').map((index, el) => $(el).attr('src')).get();
+                    let names = $(el).find('.teamname').map((index, el) => $(el).text()).get();
+                    let scores = $(el).find('.teamcode').map((index, el) => $(el).text()).get();
+                    if (logos[0]) {
+                        let match = {
+                            id,
+                            date,
+                            hometeam: {
+                                name: names[0] ? names[0] : '?',
+                                id: logos[0] ? (Number(path.basename(logos[0], '.png').split('_')[0])) : 'null',
+                                score: scores[0] ? Number(scores[0]) : 0
+                            },
+                            guesteam: {
+                                name: names[1] ? names[1] : '?',
+                                id: logos[1] ? (Number(path.basename(logos[1], '.png').split('_')[0])) : null,
+                                score: scores[1] ? Number(scores[1]) : 0
+                            }
+                        };
+                        result.push(match);
+                    }
+
+                });
+                callback(null);
+            }
+        });
+    }, function (err) {
+        if (!err) {
+            result.sort((a, b) => {
+                return a.date.valueOf() - b.date.valueOf();
+            });
+            cb(result);
+        }
+    });
+}
+
+
+app.get('/jihousai', (req, res) => {
+    get_jishousai(result => {
+        if (result) {
+            res.json({status: 200, result});
+        } else {
+            res.json({status: 404});
+        }
+    })
+});
+
 app.get('/schedule', (req, res) => {
     get_schedule((result) => {
         if (result) {
@@ -230,7 +324,7 @@ app.get('/hero_rank', (req, res) => {
             url: 'http://cgi.datamore.qq.com/datamore/smobahelper/herorank?mode=HeroRank2&userId=480299043&token=kDBm93WV&openid=owanlsnSOo_dGmbMb_kGPJHbMHmY&serverId=4060&uniqueRoleId=1360791437&unAuth=true&HeroType=0&GradeOfRank=1',
             headers: {
                 userAgent: 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E302;GameHelper',
-		Referer:'http://pvp.qq.com/act/idata_more_dev/career.html?toOpenid=owanlsnSOo_dGmbMb_kGPJHbMHmY&areaId=4&roleId=1099550493&appOpenid=oFhrws4tN9RyY40dxQV5cPy2-pp8&accessToken=10_ARtatSdEx5yDmCcRq8TXzog9rDN9eZhdXxjZqsozdaK71rUZ9Kinc93W6pv_PrvjwW3szvaY4TV97CK7mKUXm2n4wcv-58y4QTXTnQRyLoA&gameId=20001&serverName=%E5%BE%AE%E4%BF%A150%E5%8C%BA&roleLevel=30&openid=owanlsnSOo_dGmbMb_kGPJHbMHmY&userId=480299043&token=kDBm93WV&areaName=%E8%8B%B9%E6%9E%9C&roleName=SC%E2%80%A2%E6%B8%B8%E4%BE%A0&isMainRole=1&nickname=%E9%A9%AC%E5%BD%A6%E9%BE%99&uniqueRoleId=1360791437&serverId=4060&roleJob=%E6%B0%B8%E6%81%92%E9%92%BB%E7%9F%B3I'
+                Referer: 'http://pvp.qq.com/act/idata_more_dev/career.html?toOpenid=owanlsnSOo_dGmbMb_kGPJHbMHmY&areaId=4&roleId=1099550493&appOpenid=oFhrws4tN9RyY40dxQV5cPy2-pp8&accessToken=10_ARtatSdEx5yDmCcRq8TXzog9rDN9eZhdXxjZqsozdaK71rUZ9Kinc93W6pv_PrvjwW3szvaY4TV97CK7mKUXm2n4wcv-58y4QTXTnQRyLoA&gameId=20001&serverName=%E5%BE%AE%E4%BF%A150%E5%8C%BA&roleLevel=30&openid=owanlsnSOo_dGmbMb_kGPJHbMHmY&userId=480299043&token=kDBm93WV&areaName=%E8%8B%B9%E6%9E%9C&roleName=SC%E2%80%A2%E6%B8%B8%E4%BE%A0&isMainRole=1&nickname=%E9%A9%AC%E5%BD%A6%E9%BE%99&uniqueRoleId=1360791437&serverId=4060&roleJob=%E6%B0%B8%E6%81%92%E9%92%BB%E7%9F%B3I'
             }
         },
         (err, header, body) => {
